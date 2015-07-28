ecbd9ca73e755a217c89007daa601d1a7450a043
shadow_walker <shadowwalkersb@gmail.com>
2015-07-28 14:53:15 -0500

    Working

--- Extensions/GorgonExtension/__init__.py
+++ Extensions/GorgonExtension/__init__.py
@@ -8,9 +8,21 @@
 import re
 
 import chimera
+from subprocess import call
+import os
 
 def mainGorgon():
     MAINCHAIN = re.compile("^(N|CA|C)$", re.I)
-    for m in chimera.openModels.list(modelTypes=[chimera.Molecule]):
-        for a in m.atoms:
-            a.display = MAINCHAIN.match(a.name) != None
+#     for m in chimera.openModels.list(modelTypes=[chimera.Molecule]):
+#         for a in m.atoms:
+#             a.display = MAINCHAIN.match(a.name) != None    # run external process
+#     pathGorgon="/Users/durmaz/Files/eclipse_workspace/workspace_work/gitGorgonBitBucket/Gorgon/src_py/"
+    pathGorgon="/Users/durmaz/Files/eclipse_workspace/workspace_work/Gorgon/build-Unix_Makefiles/package/dist/Gorgon.app"
+    print "In mainGorgon()"
+#     os.chdir(pathGorgon)
+#     print "In path: " + pathGorgon
+#     check_call("open " + pathGorgon)
+    os.system("open " + pathGorgon)
+#     print "Tried to run gorgon.pyw"
+    print "Tried to run Gorgon.app"
+#     call(["python", pathGorgon])
